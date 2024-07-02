c6fd08d5f763a7b769a75f176ae2cbd0c4092ca5
Natalie Demyanenko <83845798+SaraDawner2000@users.noreply.github.com>
2024-07-02 11:19:14 -0500

    add inderect associations through follow requests to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,8 +29,20 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :own_photos, class_name: "Photo", foreign_key: "owner_id"
+
   has_many :comments, foreign_key: "author_id"
+
   has_many :likes, foreign_key: "fan_id"
-  has_many :sent_follow_requests, foreign_key: "sender_id"
-  has_many :received_follow_requests, foreign_key: "recipient_id"
+
+  has_many :sent_follow_requests, class_name: "FollowRequest", foreign_key: "sender_id"
+  has_many :accepted_sent_follow_requests, -> { where(status: "accepted") }, class_name: "FollowRequest", foreign_key: "sender_id"
+
+  has_many :received_follow_requests, class_name: "FollowRequest", foreign_key: "recipient_id"
+  has_many :accepted_received_follow_requests, -> { where(status: "accepted") }, class_name: "FollowRequest", foreign_key: "recipient_id"
+
+  has_many :liked_photos, through: :likes, source: :photo
+
+  has_many :leaders, through: :accepted_sent_follow_requests, source: :recipient
+
+  has_many :followers, through: :accepted_received_follow_requests, source: :sender
 end
