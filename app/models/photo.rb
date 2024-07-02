480f000d5ac5d14499c694b86a80a1b0ce95498c
Natalie Demyanenko <83845798+SaraDawner2000@users.noreply.github.com>
2024-07-02 12:08:22 -0500

    correct mistake in association

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -24,7 +24,7 @@ class Photo < ApplicationRecord
 
   belongs_to :owner, class_name: "User", counter_cache: true
 
-  has_many :comments, :likes
+  has_many :comments
 
   has_many :fans, through: :likes
 
