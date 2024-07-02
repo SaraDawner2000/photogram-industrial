03a08ec4bdcf8bb421da06a4ca1551d5e7f19b1f
Natalie Demyanenko <83845798+SaraDawner2000@users.noreply.github.com>
2024-07-02 11:29:27 -0500

    add scopes to photo model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -21,7 +21,14 @@
 #
 class Photo < ApplicationRecord
   validates :caption, :image, presence: true
+
   belongs_to :owner, class_name: "User", counter_cache: true
+
   has_many :comments, :likes
+
   has_many :fans, through: :likes
+
+  scope :past_week, -> { where(created_at: 1.week.ago...) }
+
+  scope :by_likes, -> { order(likes_count: :desc) }
 end
