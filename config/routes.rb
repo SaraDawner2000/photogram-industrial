24a172531dae099bcc4d364ab3abcd8622075f01
Natalie Demyanenko <83845798+SaraDawner2000@users.noreply.github.com>
2024-07-03 15:36:03 -0500

    refactor route to user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   root "photos#index"
 
-  get "/users/:id" => "users#show", as: :user
-
   devise_for :users
 
   resources :likes
   resources :follow_requests
   resources :comments
   resources :photos
+
+  get "/:username" => "users#show", as: :user
 end
